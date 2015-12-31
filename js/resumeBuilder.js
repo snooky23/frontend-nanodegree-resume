17750a94497f0dda809cd12b84e611382070deef
Avi Levinshtein <avi.levinshtein@ironsrc.com>
2016-01-01 00:13:01 +0200

    T1 - Build four JSON objects

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,70 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+
+//My JSON Details
+
+var bio = {
+	"name": "Avi Levinshtein",
+	"role": "Mobile Deveolpoer",
+	"contacts": {
+		"mobile": "+972-545-930806",
+		"email": "aviavi23@gmail.com",
+		"github": "https://github.com/snooky23/frontend-nanodegree-resume",
+		"location": "Tel-Avi, Isreal"
+	},
+	"welcomeMessage": "Welcome to my profile!",
+	"skills": [
+		"Good orientation in information systems",
+		"High technological abilities",
+		"Background and experience CUDA kernel , C , C++ , Java , Java on Android ,Objective C, Assembly & Matlab",
+		"Background and experience in Databases like MYSQL, Oracle, Access",
+		"Deep knowledge of Internet technologies, TCP/IP & communication protocols"
+	],
+	"biopic": "https://www.linkedin.com/in/avi-levinshtein-113a042b",
+};
+
+var education = {
+	"schools": [{
+		"name": "Afeka Collage",
+		"location": "Tel-Avi, Israel",
+		"degree": "Computer Software Engineering, Bachelor of Science (BSc)",
+		"majors": ["Mobile Systems Software, Software and Information Technology"],
+		"dates": "2011-2015",
+		"url": "http://english.afeka.ac.il/"
+	}],
+	"onlineCourses": [{
+		"title": "Front-End Developer",
+		"school": "udacity",
+		"date": "2015-2016",
+		"url": "https://www.udacity.com/course/front-end-web-developer-nanodegree--nd001",
+	}]
+};
+
+var education = {
+	"jobs": [{
+		"employer": "ironSource",
+		"title": "Mobile Developer",
+		"location": "Tel-Aviv, Israel",
+		"dates": "2015 - Present",
+		"description": "Develop comprehensive and customizable SDK's for app distribution and monetization and in charge of product releases. It includes working together with different developing teams from abroad or inside our workplace. My work environment focus mostly on Android , iOS & JavaScript."
+	}, {
+		"employer": "RRMedia",
+		"title": "Network & InfoSec Engineer",
+		"location": "Airport-City, Israel",
+		"dates": "2013 - 2015",
+		"description": "Providing technical advice and guidance for clients , Writing specification & requirements documents. System design and optimum product portfolio combinations for customers throughout all stages of integration and - testing & Implementing products and providing Tier 3 level support."
+	}]
+};
+
+var projects = {
+	"projects": [{
+		"title": "Just In Case",
+		"dates": "2014",
+		"description": "JUST IN CASE is an accident management APP. This app is meant to help you JUST IN CASE you're involved in an accident. It's offers a quick and simple way for generating a report in PDF format. The user can save Pictures, Text , GPS location etc. The application was developed with Eclipse for android ADT, Java, cloud computing, consideration and UI design and implementation models with on android OS 4.4 kitkat.",
+		"images": ["https://www.dropbox.com/s/jcum3xuqj32cc1k/Cellular%20Applications%20-%20GIC.pptx"]
+	}]
+};
+
+
+
